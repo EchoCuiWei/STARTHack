f64205ff1b10eefc36c97172008eedb8af2e9834
debenoprma <coll.marce@gmail.com>
2016-03-12 12:26:44 +0100

    menus menuts fan un vermut damunt d'un munt

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -2,10 +2,13 @@
 using System.Collections;
 
 public class MenuHandler : MonoBehaviour {
-
+    
+	public static bool isRGBKeyboard;
+	public UnityEngine.UI.Button keyboardButton;
+    
 	// Use this for initialization
 	void Start () {
-	
+	    isRGBKeyboard = true;
 	}
 	
 	// Update is called once per frame
@@ -15,5 +18,24 @@ public class MenuHandler : MonoBehaviour {
 
 	public void handleExitButton() {
         Application.Quit();
+    }
+    
+    public void handleStartButton() {
+        Application.LoadLevel("main");
+    }
+    
+    public void handleCreditsButton() {
+        Application.LoadLevel("Credits");
+    }
+    
+    public void handleKeyboardButton() {
+        isRGBKeyboard = !isRGBKeyboard;
+
+        
+        if(isRGBKeyboard) {
+			keyboardButton.GetComponentInChildren<UnityEngine.UI.Text>().text = "KEYBOARD: RGB";
+        } else {
+			keyboardButton.GetComponentInChildren<UnityEngine.UI.Text>().text = "KEYBOARD: STANDARD";
+        }
     }
 }
