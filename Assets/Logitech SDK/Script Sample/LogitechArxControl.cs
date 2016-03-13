28a0389efe814a809aa31736e06f70bfde4ac663
WolframTheta <WolframTheta@Xavier-PC>
2016-03-13 01:14:00 +0100

    no message

--- Assets/Logitech SDK/Script Sample/LogitechArxControl.cs
+++ Assets/Logitech SDK/Script Sample/LogitechArxControl.cs
@@ -1,100 +1,116 @@
-// Logitech Gaming SDK
-//
-// Copyright (C) 2011-2014 Logitech. All rights reserved.
-// Author: Tiziano Pigliucci
-// Email: devtechsupport@logitech.com
-
 using UnityEngine;
 using System.Collections;
 using System.Runtime.InteropServices;
 using System;
 using System.IO;
 
-public class LogitechArxControl : MonoBehaviour {
+public class LogitechArxControl : MonoBehaviour
+{
 
-    private GameObject player;
-    private PlayerController pc;
-	private String descriptionLabel;
-	// Use this for initialization
-	void Start () {
+    private String descriptionLabel;
+    IntPtr ctx_flags;
+    // Use this for initialization
+    GameObject player;
+    PlayerController pc;
+    void Start()
+    {
 
-		LogitechGSDK.logiArxCbContext cbContext;
-		LogitechGSDK.logiArxCB cbInstance = new LogitechGSDK.logiArxCB(this.ArxSDKCallback);
-		cbContext.arxCallBack = cbInstance;
-		cbContext.arxContext = IntPtr.Zero;
+        LogitechGSDK.logiArxCbContext cbContext;
+        LogitechGSDK.logiArxCB cbInstance = new LogitechGSDK.logiArxCB(this.ArxSDKCallback);
+        ctx_flags = Marshal.AllocHGlobal(sizeof(int));
+        cbContext.arxCallBack = cbInstance;
+        cbContext.arxContext = ctx_flags;
         player = GameObject.Find("player");
         pc = player.GetComponent<PlayerController>();
-		LogitechGSDK.LogiArxInit("com.logitech.unitysample", "Unity Sample", ref cbContext);
-		descriptionLabel = "Click the left mouse button to update the progress bar, Press G to switch to a different index file, press I to go back to the original one.";
-	}
+        LogitechGSDK.LogiArxInit("com.logitech.unitysample", "Unity Sample", ref cbContext);
+        descriptionLabel = "Click the left mouse button to update the progress bar, Press G to switch to a different index file, press I to go back to the original one.";
+    }
 
-	void OnGUI(){
-		GUI.Label(new Rect(10, 350, 500, 50), descriptionLabel);
-	}
-	// Update is called once per frame
-	void Update(){
-		if(Input.GetKey(KeyCode.Mouse0)){
-			//On left-mouse click set random value on applet's progress bar
-			System.Random random = new System.Random();
-			int val = random.Next(0, 100);
-			LogitechGSDK.LogiArxSetTagPropertyById("progressbarProgress","style.width",""+val+"%");
-
-            Debug.Log("asdsda");
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 350, 500, 50), descriptionLabel);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            //On left-mouse click set random value on applet's progress bar
+            System.Random random = new System.Random();
+            int val = random.Next(0, 100);
+            LogitechGSDK.LogiArxSetTagPropertyById("progressbarProgress", "style.width", "" + val + "%");
         }
-		///Switch index file on right-mouse down and up
-		
-	}
+        ///Switch index file on right-mouse down and up
+        if (Input.GetKey(KeyCode.I))
+        {
+            LogitechGSDK.LogiArxSetIndex("applet.html");
+        }
+        if (Input.GetKey(KeyCode.G))
+        {
+            LogitechGSDK.LogiArxSetIndex("gameover.html");
+        }
 
-	public static string getHtmlString()
-	{
-		string retStr = "";
-		retStr += "<html><center><body bgcolor='black'><a href='applet.html'><img src='gameover.png'/></a></body></center></html>";
-		return retStr;
-	}
+        if (Marshal.ReadInt32(ctx_flags) == 1)
+        {
+            Marshal.WriteInt32(ctx_flags, 0);
+            Debug.Log("Marshal works");
+            transform.position += new Vector3(1, 0, 0);
+        }
+    }
 
-	void ArxSDKCallback(int eventType, int eventValue, String eventArg, IntPtr context)
-	{ 
-		Debug.Log("CALLBACK: type:"+eventType+", value:"+eventValue+", arg:"+eventArg);
-		if(eventType ==LogitechGSDK.LOGI_ARX_EVENT_MOBILEDEVICE_ARRIVAL)
-		{
-			if(!LogitechGSDK.LogiArxAddFileAs("Assets//Logitech SDK//AppletData//applet.html","applet.html", ""))
-			{
-				Debug.Log("Could not send applet.html : "+LogitechGSDK.LogiArxGetLastError());
-			}
-            if(!LogitechGSDK.LogiArxAddFileAs("Assets//Logitech SDK//AppletData//background.png","background.png",""))
-			{
-				Debug.Log("Could not send background.png : "+LogitechGSDK.LogiArxGetLastError());
-			}
-			if(!LogitechGSDK.LogiArxAddUTF8StringAs(getHtmlString(),"gameover.html"))
-			{
-				Debug.Log("Could not send gameover.html  : "+LogitechGSDK.LogiArxGetLastError());
-			}
-			
-            byte [] gameoverImageBytes = File.ReadAllBytes("Assets//Logitech SDK//AppletData//gameover.png");
-			if(!LogitechGSDK.LogiArxAddContentAs(gameoverImageBytes, gameoverImageBytes.Length, "gameover.png"))
-			{
-				Debug.Log("Could not send gameover.png  : "+LogitechGSDK.LogiArxGetLastError());
-			}	
+    public static string getHtmlString()
+    {
+        string retStr = "";
+        retStr += "<html><center><body bgcolor='black'><a href='applet.html'><img src='gameover.png'/></a></body></center></html>";
+        return retStr;
+    }
 
-			if(!LogitechGSDK.LogiArxSetIndex("applet.html"))
-			{
-				Debug.Log("Could not set index : "+LogitechGSDK.LogiArxGetLastError());
-			}
-					
-		}
-		else if(eventType ==LogitechGSDK.LOGI_ARX_EVENT_MOBILEDEVICE_REMOVAL)
-		{
-			Debug.Log("NO DEVICES");
-		}
-		else if (eventType == LogitechGSDK.LOGI_ARX_EVENT_TAP_ON_TAG)
-		{
-			Debug.Log("Tap on tag with id :"+eventArg);
+    void ArxSDKCallback(int eventType, int eventValue, String eventArg, IntPtr context)
+    {
+
+        Marshal.WriteInt32(context, 1);
+        Debug.Log("CALLBACK: type:" + eventType + ", value:" + eventValue + ", arg:" + eventArg);
+        if (eventType == LogitechGSDK.LOGI_ARX_EVENT_MOBILEDEVICE_ARRIVAL)
+        {
+            if (!LogitechGSDK.LogiArxAddFileAs("Assets//Logitech SDK//AppletData//applet.html", "applet.html", ""))
+            {
+                Debug.Log("Could not send applet.html : " + LogitechGSDK.LogiArxGetLastError());
+            }
+            if (!LogitechGSDK.LogiArxAddFileAs("Assets//Logitech SDK//AppletData//background.png", "background.png", ""))
+            {
+                Debug.Log("Could not send background.png : " + LogitechGSDK.LogiArxGetLastError());
+            }
+            if (!LogitechGSDK.LogiArxAddUTF8StringAs(getHtmlString(), "gameover.html"))
+            {
+                Debug.Log("Could not send gameover.html  : " + LogitechGSDK.LogiArxGetLastError());
+            }
+
+            byte[] gameoverImageBytes = File.ReadAllBytes("Assets//Logitech SDK//AppletData//gameover.png");
+            if (!LogitechGSDK.LogiArxAddContentAs(gameoverImageBytes, gameoverImageBytes.Length, "gameover.png"))
+            {
+                Debug.Log("Could not send gameover.png  : " + LogitechGSDK.LogiArxGetLastError());
+            }
+
+            if (!LogitechGSDK.LogiArxSetIndex("applet.html"))
+            {
+                Debug.Log("Could not set index : " + LogitechGSDK.LogiArxGetLastError());
+            }
+
+        }
+        else if (eventType == LogitechGSDK.LOGI_ARX_EVENT_MOBILEDEVICE_REMOVAL)
+        {
+            Debug.Log("NO DEVICES");
+        }
+        else if (eventType == LogitechGSDK.LOGI_ARX_EVENT_TAP_ON_TAG)
+        {
+            Debug.Log("Tap on tag with id :" + eventArg);
             pc.attackingF();
-		}			
-	}
-	
-	void OnDestroy () {
-		//Free G-Keys SDKs before quitting the game
-		LogitechGSDK.LogiArxShutdown();
-	}
+        }
+    }
+
+    void OnDestroy()
+    {
+        //Free G-Keys SDKs before quitting the game
+        LogitechGSDK.LogiArxShutdown();
+    }
 }
